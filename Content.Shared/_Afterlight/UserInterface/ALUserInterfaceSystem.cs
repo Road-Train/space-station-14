867cd6861e134ea610fa4c8188d17b05ed82b196
DrSmugleaf <drsmugleaf@gmail.com>
2025-08-05 05:05:30 -0700

    Add ALUserInterfaceSystem.EnsureUI

---
+++ Content.Shared/_Afterlight/UserInterface/ALUserInterfaceSystem.cs
@@ -0,0 +1,15 @@
+﻿namespace Content.Shared._Afterlight.UserInterface;
+
+public sealed class ALUserInterfaceSystem : EntitySystem
+{
+    [Dependency] private readonly SharedUserInterfaceSystem _ui = default!;
+
+    public void EnsureUI(Entity<UserInterfaceComponent?> ent, Enum key, string bui)
+    {
+        if (_ui.HasUi(ent, key))
+            return;
+
+        var data = new InterfaceData(bui);
+        _ui.SetUi(ent.AsNullable(), key, data);
+    }
+}
